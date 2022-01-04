589282cc831d4e903d6467ec8e1d06663cceac7c
William Lewis <william@formative.com>
2022-01-04 15:02:07 -0500

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const DemoStyledButton = styled.button`
+  color: red;
+`;
+
+const App: React.VFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <DemoStyledButton>Click me</DemoStyledButton>
+    </main>
   );
-}
+};
 
 export default App;
